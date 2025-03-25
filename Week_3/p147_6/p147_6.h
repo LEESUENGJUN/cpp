34f1409a0754037dd843824218b06836b3703fa0
LEESUENGJUN <fpemzkdl79@naver.com>
2025-03-25 21:50:18 +0900

    first commit

--- Week_3/p147_6/p147_6.h
+++ Week_3/p147_6/p147_6.h
@@ -8,9 +8,9 @@
 
 using namespace std;
 
-// 함수 선언 (반환값: int → 점수 반환)
-int playGugu(int numQuestions);
-int playMul(int numQuestions);
-int playAdd(int digits, int numQuestions);
+// 함수 선언 
+int playGugu(int numQuestions); // 구구단 퀴즈 함수 선언
+int playMul(int numQuestions); // 곱셈 퀴즈 함수 선언
+int playAdd(int digits, int numQuestions); // 덧셈 퀴즈 함수 선언
 
 #endif 
